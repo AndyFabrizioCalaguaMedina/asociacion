318f526b6756246a583d2409f62fa12c8a76cb1a
destruyer212 <jesus121998@hotmail.com>
2025-10-18 23:35:15 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Configuración del motor de plantillas EJS
+app.set('view engine', 'ejs');
+
+// Configuración de la carpeta 'views' donde se encuentran los archivos .ejs
+app.set('views', path.join(__dirname, 'views'));
+
+// Servir archivos estáticos como CSS, imágenes, etc.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para la vista de inicio (index)
+app.get('/', (req, res) => {
+  res.render('index'); // Esto renderiza 'index.ejs'
+});
+
+// Ruta para servicios
+app.get('/servicios', (req, res) => {
+  res.render('servicios'); // Esto renderiza 'servicios.ejs'
+});
+
+// Iniciar el servidor en el puerto 3000
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
